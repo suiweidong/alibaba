2238ee1370f86391e41f35c343b864724e2707c5
团子 <7334501@qq.com>
2020-11-12 20:13:55 +0800

    nacos-config

---
+++ cloud-alibaba-config-nacos-client3377/src/main/java/com/alibaba/springcloud/AlibabaConfigNacosClient3377Application.java
@@ -0,0 +1,13 @@
+package com.alibaba.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaConfigNacosClient3377Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaConfigNacosClient3377Application.class, args);
+    }
+
+}
---
+++ cloud-alibaba-config-nacos-client3377/src/main/java/com/alibaba/springcloud/controller/NacosConfigController.java
@@ -0,0 +1,17 @@
+package com.alibaba.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NacosConfigController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/cloud/alibaba/config/getConfigInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
