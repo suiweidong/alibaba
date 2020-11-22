6faa2930875f2d0c4d2920b3a8937520f94f094e
suiweidong <7334501@qq.com>
2020-11-22 13:25:35 +0800

    项目jar包优化

---
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/alibaba/springcloud/AlibabaSentinelService8401Application.java
@@ -0,0 +1,13 @@
+package com.alibaba.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibabaSentinelService8401Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlibabaSentinelService8401Application.class, args);
+    }
+
+}
---
+++ cloud-alibaba-sentinel-service8401/src/test/java/com/alibaba/springcloud/AlibabaSentinelService8401ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlibabaSentinelService8401ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
