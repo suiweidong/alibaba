0b8511ed40c66f42598584622771956439404c66
suiweidong <7334501@qq.com>
2020-11-22 21:59:49 +0800

    sentinel与微服务 不在同一台机器 则sentinel监控不到问题 解决

--- cloud-alibaba-sentinel-service8401/src/main/java/com/alibaba/springcloud/AlibabaSentinelService8401Application.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/alibaba/springcloud/AlibabaSentinelService8401Application.java
@@ -1,5 +1,7 @@
 package com.alibaba.springcloud;
 
+import com.alibaba.csp.sentinel.transport.config.TransportConfig;
+import com.alibaba.springcloud.utils.IPUntils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +11,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class AlibabaSentinelService8401Application {
 
     public static void main(String[] args) {
+
+        String ip = IPUntils.getOutIPV4();
+        System.setProperty(TransportConfig.HEARTBEAT_CLIENT_IP, ip);
 
         SpringApplication.run(AlibabaSentinelService8401Application.class, args);
     }
