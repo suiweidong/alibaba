e0350e8af045a6a8a3f6bad92491474bb5f4b55d
suiweidong <7334501@qq.com>
2020-11-23 00:56:30 +0800

    11

--- cloud-alibaba-sentinel-service8401/src/main/java/com/alibaba/springcloud/controller/FlowLimitController.java
+++ cloud-alibaba-sentinel-service8401/src/main/java/com/alibaba/springcloud/controller/FlowLimitController.java
@@ -1,13 +1,16 @@
 package com.alibaba.springcloud.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class FlowLimitController {
 
     @GetMapping("/cloud/sentinel/testA")
     public String testA() {
+        log.info("===========:" +Thread.currentThread().getName() + "========testA");
         return "------testA";
     }
 
